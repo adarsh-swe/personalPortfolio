2ec12f1a5d74f9045b0921bb8a1bc10aeec4663f
Adarsh Patel <swe.adarsh@gmail.com>
2021-06-20 00:19:40 -0400

    create personal version

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url="localhost:5000")
+   return render_template('aboutPage.html', title="Adarsh Patel", url=os.getenv("URL"))
 
-@app.route('/health')
-def health():
-   return "Works"
+@app.route('/about')
+def about():
+    return render_template('aboutPage.html', title="Adarsh Patel" , url=os.getenv("URL"))
+
+@app.route('/blog')
+def blog():
+    return render_template('blogPage.html', title="Adarsh Patel", url=os.getenv("URL"))
+
+@app.route('/projects')
+def projects():
+    return render_template('projectsPage.html', title="Adarsh Patel", url=os.getenv("URL"))
