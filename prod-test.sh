9b9f376e50d37fc56906ecee6155e8c30e5c593d
Adarsh Patel <swe.adarsh@gmail.com>
2021-07-09 16:54:52 -0400

    added exit status to test script

--- prod-test.sh
+++ prod-test.sh
@@ -14,24 +14,27 @@ projectPage=$(curl -s -o /dev/null -I -w "%{http_code}" https://adarsh-portfolio
 
 #getting http codes for register and login 
 
+counter=0 
+flag=0
 
 echo -e "\n*** CHECKING ALL STATIC ENDPOINTS ***\n"
 #check if page loads
 curl -o - https://adarsh-portfolio.duckdns.org | grep "Adarsh Patel" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: webiste loaded${NC}"
 else
     echo -e "${RED}Fail: website failed to load${NC}" 
 fi
 
-echo $healthEndpoint
-echo $aboutPage
+counter=$((counter+flag))
 
 #health endpoint
 if [ $healthEndpoint -eq 200 ] ; then 
     echo -e "${GREEN}Success: health endpoint verified${NC}"
 else
     echo -e "${RED}Fail: health endpoint failed${NC}" 
+    counter=$((counter+1))
 fi
 
 #about page
@@ -39,6 +42,7 @@ if [ $aboutPage -eq 200 ] ; then
     echo -e "${GREEN}Success: about page loaded${NC}"
 else
     echo -e "${RED}Fail: about page failed to load${NC}" 
+    counter=$((counter+1))
 fi
 
 #blog page
@@ -46,6 +50,7 @@ if [ $blogPage -eq 200 ] ; then
     echo -e "${GREEN}Success: blog page loaded${NC}"
 else
     echo -e "${RED}Fail: blog page failed to load${NC}" 
+    counter=$((counter+1))
 fi
 
 #projects page 
@@ -53,6 +58,7 @@ if [ $projectPage -eq 200 ] ; then
     echo -e "${GREEN}Success: projects page loaded${NC}"
 else
     echo -e "${RED}Fail: projects page failed to load${NC}" 
+    counter=$((counter+1))
 fi
 
 echo -e "\n*** CHECKING REGISTER AND LOGIN ENDPOINTS ***\n"
@@ -60,27 +66,33 @@ echo -e "\n*** CHECKING REGISTER AND LOGIN ENDPOINTS ***\n"
 #user already created with username test and password test
 #test successful login
 curl -X POST -d "username=test&password=test" https://adarsh-portfolio.duckdns.org/login | grep "Login Successful" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: login with correct credentials works${NC}"
 else
     echo -e "${RED}Fail: login with correct credentials failed${NC}" 
 fi
+counter=$((counter+flag))
 
 #check if incorrect username detected
 curl -X POST -d "username=tssest&password=test" https://adarsh-portfolio.duckdns.org/login | grep "Incorrect username" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: (LOGIN) Invalid username rejected${NC}"
 else
     echo -e "${RED}Fail: (LOGIN) Invalid username not rejected${NC}" 
 fi
+counter=$((counter+flag))
 
 #check if incorrect password detected
 curl -X POST -d "username=test&password=tessst" https://adarsh-portfolio.duckdns.org/login | grep "Incorrect password" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: (LOGIN) Incorrect password rejected${NC}"
 else
     echo -e "${RED}Fail: (LOGIN) Incorrect password rejected${NC}" 
 fi
+counter=$((counter+flag))
 
 #testing the register endpoint 
 
@@ -88,24 +100,32 @@ curl -X POST -d "username=test&password=test" https://adarsh-portfolio.duckdns.o
 
 #check if empty username is detected 
 curl -X POST -d "username=&password=tessst" https://adarsh-portfolio.duckdns.org/register | grep "Username is required" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: (REGISTER) Empty username feild detected${NC}"
 else
     echo -e "${RED}Fail: (REGISTER) Empty username feild not detected${NC}" 
 fi
+counter=$((counter+flag))
 
 #check if empty password is detected 
 curl -X POST -d "username=test&password=" https://adarsh-portfolio.duckdns.org/register | grep "Password is required" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: (REGISTER) Empty password feild detected${NC}"
 else
     echo -e "${RED}Fail: (REGISTER) Empty password feild not detected${NC}" 
 fi
+counter=$((counter+flag))
 
 #check if existing user found by using 'test' user previously created 
 curl -X POST -d "username=test&password=test" https://adarsh-portfolio.duckdns.org/register | grep "User test is already registered" -q
-if [ $? -eq 0 ] ; then
+flag=$?
+if [ $flag -eq 0 ] ; then
     echo -e "${GREEN}Success: (REGISTER) Detected username already in use${NC}"
 else
     echo -e "${RED}Fail: (REGISTER) Failed to detect username already in use${NC}" 
 fi
+counter=$((counter+flag))
+
+exit $counter
